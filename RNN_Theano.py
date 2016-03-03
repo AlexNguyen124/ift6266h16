4ab1c0c49433156260b6dcd7885feec50d18043a
AlexNguyen124 <alexandre.nguyen@hec.ca>
2016-03-03 17:42:33 -0500

    Update RNN_Theano.py

--- RNN_Theano.py
+++ RNN_Theano.py
@@ -6,45 +6,58 @@ Adaptation of https://github.com/dennybritz/rnn-tutorial-rnnlm/blob/master/rnn_t
 import numpy as np
 import theano as theano
 import theano.tensor as T
-from utils import *
-import operator
+
+
+input_dim=10
+hidden_dim=100
 
 class RNNTheano:
-    
-    def __init__(self, seq_dim, hidden_dim=100, bptt_truncate=4):
-        # Assign instance variables
-        self.seq_dim = seq_dim #Dimension of a sequence of X
-        self.hidden_dim = hidden_dim
-        #self.bptt_truncate = bptt_truncate #To use for timeshift?
-        
-        # Randomly initialize the network parameters
-        U = np.random.uniform(-np.sqrt(1./seq_dim), np.sqrt(1./seq_dim), (hidden_dim, seq_dim))
-        V = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (seq_dim, hidden_dim))
-        W = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (hidden_dim, hidden_dim))
+    #initialize everything!
+    def __init__(self, input, target, input_dim, hidden_dim, output_dim, lr):
+        self.input  = input
+        self.target = target
+        self.lr     = lr
+        #Weight Matrixes        
+        U           = np.random.uniform(size=(hidden_dim, input_dim),  low=-np.sqrt(1./input_dim),  high=np.sqrt(1./input_dim),  dtype=theano.config.floatX)  # input-to-hidden weight matrix
+        W           = np.random.uniform(size=(hidden_dim, hidden_dim), low=-np.sqrt(1./hidden_dim), high=np.sqrt(1./hidden_dim), dtype=theano.config.floatX) # Hidden to hidden recurrent connections weight matrix
+        V           = np.random.uniform(size=(input_dim, hidden_dim),  low=-np.sqrt(1./hidden_dim), high=np.sqrt(1./hidden_dim), dtype=theano.config.floatX) # hidden-to-output weight matrix
+        # Biases        
+        b           = np.zeros((hidden_dim,), dtype=theano.config.floatX) # Hidden bias
+        c           = np.zeros((output_dim,), dtype=theano.config.floatX) # Output bias
+        h0          = np.zeros((hidden_dim,), dtype=theano.config.floatX) # Initial hidden State of RNN
 
+        
         # Theano: Created shared variables
-        self.U = theano.shared(name='U', value=U.astype(theano.config.floatX))
-        self.V = theano.shared(name='V', value=V.astype(theano.config.floatX))
-        self.W = theano.shared(name='W', value=W.astype(theano.config.floatX))      
+        self.U      = theano.shared(name='U', value=U)
+        self.W      = theano.shared(name='W', value=W)
+        self.V      = theano.shared(name='V', value=V)                                 
+        self.b      = theano.shared(name='b', value=b)
+        self.c      = theano.shared(name='c', value=c)
+        self.h0     = theano.shared(name='h0', value=h0)
+
+
+        self.params = [self.W, self.U, self.V, self.h0, self.b, self.c]
         
-        # We store the Theano graph here
-        self.theano = {}
-        self.__theano_build__()
+        # Recurrent activation function        
+        def step(x_t, h_tm1):
+            h_t     = T.tanh(T.dot(x_t, self.U) + T.dot(h_tm1, self.W) + self.b)
+            y_t     = T.dot(h_t, V)
+            return h_t, y_t
+        # the hidden state `h` for the entire sequence, and the output for the
+
+        [self.h, self.y], _ = theano.scan(step,
+                        sequences       =self.input,
+                        outputs_info    =[self.h0, None],
+                        non_sequences   =[W, U, V])
         
-    def __theano_build__(self):
-        U, V, W = self.U, self.V, self.W
-        x = T.ivector('x')
-        y = T.ivector('y')
-        def forward_prop_step(x_t, s_t_prev, U, V, W):
-            s_t = T.tanh(U[:,x_t] + W.dot(s_t_prev)) #How big is s_t_prev? t=1 or t=inf?
-            o_t = T.nnet.softmax(V.dot(s_t))
-            return [o_t[0], s_t]
-        [o,s], updates = theano.scan(
-            forward_prop_step,
-            sequences=x,
-            outputs_info=[None, dict(initial=T.zeros(self.hidden_dim))],
-            non_sequences=[U, V, W],
-            truncate_gradient=self.bptt_truncate,
-            strict=True)
-        
-        
+        #Error function
+        self.loss = T.mean((self.y - self.target) ** 2)
+        # gradients on the weights using BPTT
+        gW, gU, gV = T.grad(self.loss, [W, U, V])
+        # training function, that computes the error and updates the weights using
+        # SGD.
+        fn = theano.function([h0, self.input, self.target, self.lr],
+                         self.loss,
+                         updates={W: W - self.lr * gW,
+                                  U: U - self.lr * gU,
+                                  V: V - self.lr * gV})
